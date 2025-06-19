0d8b020002bf2fb21548888a6cc2ed7ba5329bb9
Poppy Khim <mobilelegendpoppykhim@gmail.com>
2025-06-19 16:20:06 +0700

    Submit

--- src/db/MyDataBase.java
+++ src/db/MyDataBase.java
@@ -1,13 +1,31 @@
 package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class MyDataBase {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/test";
-    private static final String USER = "root";
-    private static final String PASS = "Rith120223";
+    private static  String URL;
+    private static  String USER;
+    private static  String PASS;
+
+    static {
+        try (FileInputStream fis = new FileInputStream("config/db.properties")) {
+            Properties props = new Properties();
+            props.load(fis);
+
+            URL = "jdbc:mysql://localhost:3306/test";
+            USER = "root";
+            PASS = props.getProperty("db.password");
+
+        } catch (IOException e) {
+            System.out.println("Failed to load DB config: " + e.getMessage());
+        }
+    }
 
     public static Connection getConnection() {
         try {
